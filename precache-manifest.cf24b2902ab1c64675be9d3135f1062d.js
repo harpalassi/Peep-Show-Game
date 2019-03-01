ddd8f42aebda05fe354e1a9da3c04fafc38ac65a
Speedway <harpalassi@gmail.com>
2019-03-01 16:59:33 -0600

    Updates

---
+++ precache-manifest.cf24b2902ab1c64675be9d3135f1062d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Click-Memory-Game/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "5d61534c813bee9bcb25",
+    "url": "/Click-Memory-Game/static/js/runtime~main.5d61534c.js"
+  },
+  {
+    "revision": "e04bf366f67d939b52ad",
+    "url": "/Click-Memory-Game/static/js/main.e04bf366.chunk.js"
+  },
+  {
+    "revision": "5bf9b2cda7c63b962686",
+    "url": "/Click-Memory-Game/static/js/2.5bf9b2cd.chunk.js"
+  },
+  {
+    "revision": "e04bf366f67d939b52ad",
+    "url": "/Click-Memory-Game/static/css/main.549139a1.chunk.css"
+  },
+  {
+    "revision": "c84dea6829647db3a0621f4535afa9eb",
+    "url": "/Click-Memory-Game/index.html"
+  }
+];
