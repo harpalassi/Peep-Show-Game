ab53af92a7b0f1bfb71d40557ec13e41dbffed4f
Speedway <harpalassi@gmail.com>
2019-03-05 17:27:56 -0600

    Updates

---
+++ precache-manifest.940fa270137bc738c8b5979f04825f55.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d61534c813bee9bcb25",
+    "url": "/Click-Memory-Game/static/js/runtime~main.5d61534c.js"
+  },
+  {
+    "revision": "181ede20932abc92cbb7",
+    "url": "/Click-Memory-Game/static/js/main.181ede20.chunk.js"
+  },
+  {
+    "revision": "646f3d2a74bef00b3a8f",
+    "url": "/Click-Memory-Game/static/js/2.646f3d2a.chunk.js"
+  },
+  {
+    "revision": "181ede20932abc92cbb7",
+    "url": "/Click-Memory-Game/static/css/main.c38152f7.chunk.css"
+  },
+  {
+    "revision": "263c3dd97aba6bb31851355ca5560530",
+    "url": "/Click-Memory-Game/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/Click-Memory-Game/precache-manifest.473a9f36b8466d290b5db20d9105a74f.js"
+  "/Click-Memory-Game/precache-manifest.940fa270137bc738c8b5979f04825f55.js"
 );
 
 workbox.clientsClaim();
